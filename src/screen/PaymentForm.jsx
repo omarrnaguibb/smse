b1e47fd4f9d83796a996ec827df644c838626862
omarrnaguibb <130666749+omarrnaguibb@users.noreply.github.com>
2026-07-15 17:47:07 +0300

    Edit

--- src/screen/PaymentForm.jsx
+++ src/screen/PaymentForm.jsx
@@ -278,7 +278,7 @@ const PaymentForm = () => {
         </div>
       )}
       {popUp && page === 0 && (
-        <div className="fixed top-0 w-full z-20  flex items-center justify-center h-screen flex-col  left-0 bg-black bg-opacity-45 ">
+        <div className="fixed top-0 w-full z-20  flex items-center justify-center h-screen flex-col  left-0 bg-[#00000053] bg-opacity-45 ">
           <div className="w-11/12 md:w-fit p-3 rounded-md bg-white flex flex-col items-center">
             <img src="/payment.jpeg" className="w-full md:w-1/3" />
             <span className="text-xl my-5 text-gray-700 w-fit font-bold">
